8e4d05ce15f80c1dbf027fcc55494a5f462937c5
Fishelll <vladimirbalashov03@gmail.com>
2023-11-15 20:15:20 +0200

    add new vers

--- lab6.cpp
+++ lab6.cpp
@@ -1,22 +1,19 @@
 #include <iostream> 
 #include <iomanip> 
 #include <math.h> 
-#include <windows.h> 
-using namespace std; 
-int main() 
-{ 
-    SetConsoleCP(1251); 
-    SetConsoleOutputCP(1251); 
-    float a,b, d = 1, h = 0.1, c = 2; // d(початкове Х), h(крок), c(кінцеве X) 
-    cout << "Введіть значення a: "; 
-    cin >> a; 
-    cout << "Введіть значення b: "; 
-    cin >> b; 
-    float y, x = d; 
-    while (x <= c) { 
-        y = a * b + pow(sin(x), 2) - x * 1 / 2; 
-        cout << setw(10) << "x= " << x << setw(10) << "y= " << y << endl; 
-        x += h; 
-    } 
-    return 0; 
+
+int main()
+{
+    float a, b, d = 1, h = 0.1, c = 2; // d(початкове Х), h(крок), c(кінцеве X) 
+    std::cout << "Enter value a: ";
+    std::cin >> a;
+    std::cout << "Enter value b: ";
+    std::cin >> b;
+    float y, x = d;
+    while (x <= c) {
+        y = a * b + std::pow(std::sin(x), 2) - x * 1 / 2;
+        std::cout << std::setw(10) << "x= " << x << std::setw(10) << "y= " << y << std::endl;
+        x += h;
+    }
+    return 0;
 }
